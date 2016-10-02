b4080e49f3603e9d47897e2a87a3e433fba1c70d
Courtney Wright <courtney.wright@mail.mcgill.ca>
2016-10-02 17:54:14 -0400

    Upload Correction
    Uploaded incorrect file. Here is updated odometry correction file.

--- DpmLab2/src/ev3Odometer/OdometryCorrection.java
+++ DpmLab2/src/ev3Odometer/OdometryCorrection.java
@@ -3,9 +3,16 @@
  */
 package ev3Odometer;
 
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+
 public class OdometryCorrection extends Thread {
 	private static final long CORRECTION_PERIOD = 10;
 	private Odometer odometer;
+	private SensorModes lightSensor;
+	
+	private double odometerX = 0;
+	private double odometerY = 0;
 
 	// constructor
 	public OdometryCorrection(Odometer odometer) {
@@ -15,11 +22,25 @@ public class OdometryCorrection extends Thread {
 	// run method (required for Thread)
 	public void run() {
 		long correctionStart, correctionEnd;
+		
+		SampleProvider sensor = lightSensor.getMode("Red");
+		int dataSize = sensor.sampleSize();
 
 		while (true) {
 			correctionStart = System.currentTimeMillis();
 
 			// put your correction code here
+			float sensorData[] = new float[dataSize];
+			sensor.fetchSample(sensorData, 0);
+			
+			if(odometer.getTheta() < 1.5 && sensorData[0] < 0.2) {
+				
+			} else if ((odometer.getTheta() >= 1.5 && odometer.getTheta() < 3) && sensorData[0] < 0.2) {
+				
+			} else if ((odometer.getTheta() >= 3 && odometer.getTheta() < 6.2) && sensorData[0] < 0.2) {
+				
+			}
+			
 
 			// this ensure the odometry correction occurs only once every period
 			correctionEnd = System.currentTimeMillis();
